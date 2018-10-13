142191d3c01733f6d4badff6a2510b4e33193a73
Polar <masonmann2020@gmail.com>
2018-10-12 21:45:37 -0700

    Reimported OpenCV to solve errors, started sampling code
    OpenCV import was fixed by 5484 Enderbots

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MasonOpenCVTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MasonOpenCVTesting.java
@@ -1,41 +1,75 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.hardware.minibot.Robot;
-import org.firstinspires.ftc.teamcode.support.YieldHandler;
+import org.firstinspires.ftc.teamcode.components.Robot;
+import org.firstinspires.ftc.teamcode.hardware.ruckus.ToboRuckus;
+import org.firstinspires.ftc.teamcode.support.Logger;
+import org.firstinspires.ftc.teamcode.support.diagnostics.Adjuster;
+import org.firstinspires.ftc.teamcode.support.diagnostics.GamepadListener;
+import org.firstinspires.ftc.teamcode.support.diagnostics.Menu;
+import org.firstinspires.ftc.teamcode.support.diagnostics.MenuEntry;
+import org.firstinspires.ftc.teamcode.support.events.Button;
+import org.firstinspires.ftc.teamcode.support.events.EventManager;
+import org.firstinspires.ftc.teamcode.support.events.Events;
+import org.firstinspires.ftc.teamcode.support.hardware.Configuration;
 
-/**
- * Created by 28761 on 9/15/2018.
- */
+import java.lang.reflect.InvocationTargetException;
 
-@Autonomous(name = "OpenCV Test", group = "MiniBot")
-public class MasonOpenCVTesting extends LinearOpMode implements YieldHandler {
+@Autonomous(name = "Ruckus :: OpenCV Testing", group = "Ruckus")
+public class MasonOpenCVTesting extends LinearOpMode {
+    protected static int LOG_LEVEL = Log.VERBOSE;
 
-    Robot robot;
+    private Configuration configuration;
+    private Logger<Logger> log = new Logger<Logger>().configureLogging(getClass().getSimpleName(), LOG_LEVEL);
 
     @Override
     public void runOpMode() throws InterruptedException {
-        robot = new Robot();
-        robot.core.set_yield_handler(this);
-        //robot.init(hardwareMap);
+        telemetry.addData("Initializing Robot", "Please Wait ...");
+        telemetry.update();
+
+        ToboRuckus robot = new ToboRuckus().configureLogging("ToboRuckus", LOG_LEVEL);
+        configuration = new Configuration(hardwareMap, robot.getName()).configureLogging("Config", LOG_LEVEL);
+
+        try {
+            // configure robot and reset all hardware
+            robot.configure(configuration, telemetry);
+            configuration.apply();
+            robot.reset();
+
+            telemetry.addData("Robot is ready", "Press Play");
+            telemetry.update();
+        } catch (Exception E) {
+            telemetry.addData("Init Failed", E.getMessage());
+            handleException(E);
+        }
+
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        timeStart = System.currentTimeMillis();
-        //robot.chassis.drive_time_without_imu(0.4, 6000);
+        resetStartTime();
+
+        // run until driver presses STOP or runtime exceeds 30 seconds
+        while (opModeIsActive() && getRuntime() < 30) {
+            try {
+
+            } catch (Exception E) {
+                telemetry.addData("Error", E.getMessage());
+                handleException(E);
+                Thread.sleep(5000);
+            }
+        }
     }
 
-    long timeStart;
-    int statge;
-
-    long lapsed_time() {
-        return System.currentTimeMillis() - timeStart;
-    }
-
-    @Override
-    public void on_yield() {
-        telemetry.addLine("on_yield at time"+lapsed_time());
-
+    protected void handleException(Throwable T) {
+        log.error(T.getMessage(), T);
+        int linesToShow = 5;
+        for(StackTraceElement line : T.getStackTrace()) {
+            telemetry.log().add("%s.%s():%d", line.getClassName(), line.getMethodName(), line.getLineNumber());
+            if (--linesToShow == 0) break;
+        }
         telemetry.update();
     }
 }
