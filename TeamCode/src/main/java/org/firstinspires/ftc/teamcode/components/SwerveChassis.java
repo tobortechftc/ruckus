354b3dadfaa854e46bb3ad202adf6dec092947cc
NickDeGrood <nick.degrood@gmail.com>
2019-01-06 17:04:01 -0800

    Disable range telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/SwerveChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/SwerveChassis.java
@@ -74,7 +74,7 @@ public class SwerveChassis extends Logger<SwerveChassis> implements Configurable
 
     private boolean useScalePower = true;//
     private boolean setImuTelemetry = false;//unless debugging, don't set telemetry for imu
-    private boolean setRangeSensorTelemetry = true;//unless debugging, don't set telemetry for range sensor
+    private boolean setRangeSensorTelemetry = false;//unless debugging, don't set telemetry for range sensor
 
     public void enableRangeSensorTelemetry() { // must be call before reset() or setupTelemetry()
         setRangeSensorTelemetry = true;
