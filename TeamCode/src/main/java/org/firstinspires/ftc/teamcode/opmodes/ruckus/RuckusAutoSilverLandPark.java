7870d9afb12466536425541e496a528cca04d3f2
kunhantsai <kunhantsai@gmail.com>
2019-03-07 02:48:42 -0800

    ensure auto programs stop w/o exceptions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ruckus/RuckusAutoSilverLandPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ruckus/RuckusAutoSilverLandPark.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes.ruckus;
 import android.util.Log;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.components.SwerveChassis;
@@ -15,7 +16,7 @@ import org.firstinspires.ftc.teamcode.support.hardware.Configuration;
 /**
  * Created by 28761 on 10/13/2018.
  */
-
+@Disabled
 @Autonomous(name = "Auto-Silver-Land-Park", group = "Ruckus")
 public class RuckusAutoSilverLandPark extends LinearOpMode implements YieldHandler {
     protected static int LOG_LEVEL = Log.VERBOSE;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ruckus/RuckusHopefullyTheFinalGoldAutonomousForState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ruckus/RuckusHopefullyTheFinalGoldAutonomousForState.java
@@ -26,39 +26,45 @@ public class RuckusHopefullyTheFinalGoldAutonomousForState extends LinearOpMode 
 
     @Override
     public void runOpMode() throws InterruptedException {
-        telemetry.addData("Initializing Robot", "Please Wait ...");
-        telemetry.update();
+        ToboRuckus robot = null;
 
-        ToboRuckus robot = new ToboRuckus().configureLogging("ToboRuckus", LOG_LEVEL);
-        configuration = new Configuration(hardwareMap, robot.getName()).configureLogging("Config", LOG_LEVEL);
+        if (opModeIsActive()) {
 
-        try {
-            // configure robot and reset all hardware
-            robot.configure(configuration, telemetry, true);
-            configuration.apply();
-            robot.reset(true);
-
-            telemetry.addData("Robot is ready", "Press Play");
+            telemetry.addData("Initializing Robot", "Please Wait ...");
             telemetry.update();
-        } catch (Exception E) {
-            telemetry.addData("Init Failed", E.getMessage());
-            handleException(E);
-        }
 
+            robot = new ToboRuckus().configureLogging("ToboRuckus", LOG_LEVEL);
+            configuration = new Configuration(hardwareMap, robot.getName()).configureLogging("Config", LOG_LEVEL);
+
+            try {
+                // configure robot and reset all hardware
+                robot.configure(configuration, telemetry, true);
+                configuration.apply();
+                robot.reset(true);
+
+                telemetry.addData("Robot is ready", "Press Play");
+                telemetry.update();
+            } catch (Exception E) {
+                telemetry.addData("Init Failed", E.getMessage());
+                handleException(E);
+            }
+        }
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        resetStartTime();
-        robot.core.set_yield_handler(this); // uses this class as yield handler
 
+        if (opModeIsActive()) {
 
-        // Step-1: check random sample position
-        ToboRuckus.MineralDetection.SampleLocation sam_loc = ToboRuckus.MineralDetection.SampleLocation.CENTER;
-        sam_loc = robot.cameraMineralDetector.getGoldPositionTF(true);
+            resetStartTime();
+            robot.core.set_yield_handler(this); // uses this class as yield handler
 
-        // Step-2: landing mission
-        robot.hopefullyTheLastLandAndDetachForState(null, false);
+            // Step-1: check random sample position
+            ToboRuckus.MineralDetection.SampleLocation sam_loc = ToboRuckus.MineralDetection.SampleLocation.CENTER;
+            sam_loc = robot.cameraMineralDetector.getGoldPositionTF(true);
 
-        //step-3: retrieve sample
+            // Step-2: landing mission
+            robot.hopefullyTheLastLandAndDetachForState(null, false);
+
+            //step-3: retrieve sample
 //        switch (sam_loc) {
 //            case CENTER: // center
 //                robot.chassis.rotateTo(0.4, -90);
@@ -79,22 +85,23 @@ public class RuckusHopefullyTheFinalGoldAutonomousForState extends LinearOpMode 
 //        robot.autoTransfer();
 //        if (sam_loc != ToboRuckus.MineralDetection.SampleLocation.CENTER)
 //            robot.chassis.rotateTo(0.4, -90);
-        robot.collectSampleAndGoToWall(sam_loc);
-        //step-4: deliver marker
+            robot.collectSampleAndGoToWall(sam_loc);
+            //step-4: deliver marker
 //        robot.chassis.driveStraightAuto(0.4, 90, -64, 3000);
-        Thread.sleep(200);
-        robot.chassis.rotateTo(0.4, +135);
-        Thread.sleep(200);
-        robot.chassis.driveStraightAuto(0.2, robot.chassis.getDistance(SwerveChassis.Direction.RIGHT) - 6, +90, 1000);
-        if (sam_loc == ToboRuckus.MineralDetection.SampleLocation.CENTER)
-            robot.chassis.driveStraightAuto(0.4, -75, 0, 3000);
-        else
-            robot.chassis.driveStraightAuto(0.4, -85, 0, 3000);
-        robot.hanging.markerDown();
-        Thread.sleep(200);
-        //step-5: go parking
-        robot.chassis.driveStraightAuto(0.3, 20, 5, 500);
-        robot.goParking(ToboRuckus.Side.GOLD);
+            Thread.sleep(200);
+            robot.chassis.rotateTo(0.4, +135);
+            Thread.sleep(200);
+            robot.chassis.driveStraightAuto(0.2, robot.chassis.getDistance(SwerveChassis.Direction.RIGHT) - 6, +90, 1000);
+            if (sam_loc == ToboRuckus.MineralDetection.SampleLocation.CENTER)
+                robot.chassis.driveStraightAuto(0.4, -75, 0, 3000);
+            else
+                robot.chassis.driveStraightAuto(0.4, -85, 0, 3000);
+            robot.hanging.markerDown();
+            Thread.sleep(200);
+            //step-5: go parking
+            robot.chassis.driveStraightAuto(0.3, 20, 5, 500);
+            robot.goParking(ToboRuckus.Side.GOLD);
+        }
     }
 
     protected void handleException(Throwable T) {
