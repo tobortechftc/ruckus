78071d7478acedec4131984019d7bc5252effc30
Alpha-kun <CharlieLiuAlpha@gmail.com>
2019-06-15 14:44:00 -0700

    re-checkin mecbot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/MechChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/MechChassis.java
@@ -1,8 +1,53 @@
 package org.firstinspires.ftc.teamcode.components;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.support.hardware.Configuration;
+
 /**
  * Created by 28761 on 6/14/2019.
  */
 
 public class MechChassis {
+
+    DcMotor motorFL;
+    DcMotor motorFR;
+    DcMotor motorBL;
+    DcMotor motorBR;
+
+    public MechChassis(HardwareMap hardwareMap) {
+        motorFL = hardwareMap.tryGet(DcMotor.class, "motorFL");
+        motorFR = hardwareMap.tryGet(DcMotor.class, "motorFR");
+        motorBL = hardwareMap.tryGet(DcMotor.class, "motorBL");
+        motorBR = hardwareMap.tryGet(DcMotor.class, "motorBR");
+    }
+
+    public void yMove(int sgn) {
+        motorFL.setPower(sgn*0.5);
+        motorFR.setPower(-sgn*0.5);
+        motorBL.setPower(sgn*0.5);
+        motorBR.setPower(-sgn*0.5);
+    }
+
+    public void xMove(int sgn) {
+        motorFL.setPower(sgn*0.5);
+        motorFR.setPower(sgn*0.5);
+        motorBL.setPower(-sgn*0.5);
+        motorBR.setPower(-sgn*0.5);
+    }
+
+    public void turn(int sgn) {
+        motorFL.setPower(sgn*0.5);
+        motorFR.setPower(sgn*0.5);
+        motorBL.setPower(sgn*0.5);
+        motorBR.setPower(sgn*0.5);
+    }
+
+    public void carDrive(int sgn) {
+        motorFL.setPower(sgn*0.5);
+        motorFR.setPower(sgn*0.5);
+        motorBL.setPower(sgn*0.5);
+        motorBR.setPower(sgn*0.5);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/mechBot/mechTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/mechBot/mechTest.java
@@ -1,8 +1,41 @@
 package org.firstinspires.ftc.teamcode.opmodes.mechBot;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.components.MechChassis;
+
 /**
  * Created by 28761 on 6/14/2019.
  */
 
-public class mechTest {
+@Autonomous(name = "TEST-MECH", group = "Ruckus")
+public class mechTest  extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        telemetry.addData("Initializing Robot", "Please Wait ...");
+        telemetry.update();
+
+        MechChassis mc=new MechChassis(hardwareMap);
+
+        telemetry.addData("Robot is Initialized", "ready for start");
+        telemetry.update();
+
+        waitForStart();
+
+        while(true){
+            if(gamepad1.dpad_up){
+                mc.yMove(+1);
+            }else if(gamepad1.dpad_down){
+                mc.yMove(-1);
+            }else if(gamepad1.dpad_right){
+                mc.xMove(+1);
+            }else if(gamepad1.dpad_left){
+                mc.xMove(-1);
+            }else{
+                mc.yMove(0);
+            }
+        }
+    }
 }
